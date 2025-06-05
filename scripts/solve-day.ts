aec520b951124b66041304b18256aa1628f6e0f3
Mr-Unforgettable <abhinavpsingh2024@protonmail.com>
2025-06-05 21:58:11 +0530

    Auto formatting.

--- scripts/solve-day.ts
+++ scripts/solve-day.ts
@@ -1,14 +1,14 @@
 const day = Deno.args[0];
 if (!day) {
-    console.error("❌ Usage: deno task test-day <DAY>");
-    Deno.exit(1);
+  console.error("❌ Usage: deno task test-day <DAY>");
+  Deno.exit(1);
 }
 
 const command = new Deno.Command("deno", {
-    args: ["run", "--allow-read", `days/day${day}/main.ts`],
-    stdin: "inherit",
-    stdout: "inherit",
-    stderr: "inherit",
+  args: ["run", "--allow-read", `days/day${day}/main.ts`],
+  stdin: "inherit",
+  stdout: "inherit",
+  stderr: "inherit",
 });
 
 const { code } = await command.spawn().status;
